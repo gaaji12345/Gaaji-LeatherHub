c34239b9d77501a8263c89c31e92fd56bf872265
gaji12345 <gajimax12345@gmail.com>
2024-10-02 16:50:28 +0530

    exeption class done

---
+++ src/main/java/com/example/GaajiShoe/util/exeption/InvalidCredentialsException.java
@@ -0,0 +1,10 @@
+package com.example.GaajiShoe.util.exeption;/*  gaajiCode
+    99
+    09/09/2024
+    */
+
+public class InvalidCredentialsException extends RuntimeException{
+    public InvalidCredentialsException(String message){
+        super(message);
+    }
+}
---
+++ src/main/java/com/example/GaajiShoe/util/exeption/NotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.GaajiShoe.util.exeption;/*  gaajiCode
+    99
+    09/09/2024
+    */
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message){
+        super(message);
+    }
+}
