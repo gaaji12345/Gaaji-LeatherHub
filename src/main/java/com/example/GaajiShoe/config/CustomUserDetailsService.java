5345b4eabec54b1f5bbb539a37ad78b49145da99
gaji12345 <gajimax12345@gmail.com>
2024-10-02 16:57:18 +0530

    CustomUserDetailsService done

---
+++ src/main/java/com/example/GaajiShoe/config/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.example.GaajiShoe.config;/*  gaajiCode
+    99
+    09/09/2024
+    */
+
+
+import com.example.GaajiShoe.entity.User;
+import com.example.GaajiShoe.repo.UserRepo;
+import com.example.GaajiShoe.util.exeption.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepo userRepo;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepo.findByEmail(username)
+                .orElseThrow(()-> new NotFoundException("User/ Email Not found"));
+
+        return AuthUser.builder()
+                .user(user)
+                .build();
+    }
+}
