12c6ba94c9560d76f8d692ae7b08ecaf22edeec4
gaji12345 <gajimax12345@gmail.com>
2024-10-12 08:30:54 +0530

    SupplierDTO done

---
+++ src/main/java/com/example/GaajiShoe/dto/SupplierDTO.java
@@ -0,0 +1,35 @@
+package com.example.GaajiShoe.dto;/*  gaajiCode
+    99
+    12/10/2024
+    */
+
+import com.example.GaajiShoe.enums.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class SupplierDTO {
+    private String supplierCode;
+
+    private String supplierName;
+
+    private Category category;
+
+    private String addressLine01;
+
+    private String addressLine02;
+    private String addressLine03;
+    private String addressLine04;
+    private String addressLine05;
+    private String addressLine06;
+
+    private String contactNo1;
+
+    private String landLineNo;
+
+    private String email;
+
+}
